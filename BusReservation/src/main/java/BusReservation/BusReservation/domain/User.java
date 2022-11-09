0c19acf5f08d60d94e1bea7f512215a2f96b4cba
Yang-soeun <soeun8636@naver.com>
2022-11-09 20:07:03 +0900

    DB

---
+++ BusReservation/src/main/java/BusReservation/BusReservation/domain/User.java
@@ -0,0 +1,21 @@
+package BusReservation.BusReservation.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @Column(name ="user_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private Integer age;
+    @Column(nullable = false)
+    private String phone;
+    @Column(nullable = false)
+    private String password;
+}
